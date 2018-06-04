bcc287b830ac11491438791a748695c846b6553a
jlopez <jlopez@qashops>
2018-06-04 08:16:00 +0200

    hello world

--- hello/hello.go
+++ hello/hello.go
@@ -9,5 +9,8 @@ func main() {
 const HELLO = "Hello, "
 
 func HelloWorld(name string) string {
+	if name == "" {
+		name = "World"
+	}
 	return HELLO + name
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,24 @@ package main
 import "testing"
 
 func TestHelloWorld(t *testing.T) {
-	expected := "Hello, Jesús"
-	got := HelloWorld("Jesús")
 
-	if expected != got {
-		t.Errorf("got '%s' expected '%s'", got, expected)
+	assertIsCorrectMessage := func(got string, expected string, t *testing.T) {
+		t.Helper()
+		if got != expected {
+			t.Errorf("got '%s' expected '%s'", got, expected)
+		}
 	}
+	t.Run("when say hello with a name then response is hello name", func(t *testing.T) {
+		expected := "Hello, Jesús"
+		got := HelloWorld("Jesús")
+
+		assertIsCorrectMessage(got, expected, t)
+	})
+
+	t.Run("when say hello with empty name then response is say hello world", func(t *testing.T) {
+		expected := "Hello, World"
+		got := HelloWorld("")
+
+		assertIsCorrectMessage(got, expected, t)
+	})
 }
