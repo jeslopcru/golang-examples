88943a4230669e0374c30ab98d3c52c2b9bab5f3
jlopez <jlopez@qashops>
2018-08-21 22:33:59 +0200

    create a hello world package an a main package

---
+++ 00-main/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jeslopcru/golang-examples/01-hello"
+)
+
+func main() {
+	fmt.Println(hello.HelloWorld("Jesús"))
+}
--- 01-hello/hello.go
+++ 01-hello/hello.go
@@ -1,10 +1,4 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println(HelloWorld("Jesús"))
-}
+package hello
 
 const HELLO = "Hello, "
 
