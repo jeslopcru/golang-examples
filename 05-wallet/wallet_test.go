f60d93a3c2f8cc057565917f906c1655937387fb
jlopez <jlopez@qashops>
2018-10-21 16:23:14 +0200

    withdraw refactor

--- 05-wallet/wallet_test.go
+++ 05-wallet/wallet_test.go
@@ -6,23 +6,22 @@ import (
 
 func TestWallet(t *testing.T) {
 
-	t.Run("Balance", func(t *testing.T) {
-		wallet := Wallet{}
-		wallet.Deposit(Euro(10))
+	assertBalance := func(t *testing.T, wallet Wallet, expected Euro) {
 		result := wallet.Balance()
-		expected := Euro(10)
 		if expected != result {
 			t.Errorf("result %s and expected %s", result, expected)
 		}
+	}
+
+	t.Run("Balance", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Euro(10))
+		assertBalance(t, wallet, Euro(10))
 	})
 
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Euro(20)}
 		wallet.Withdraw(Euro(10))
-		result := wallet.Balance()
-		expected := Euro(10)
-		if expected != result {
-			t.Errorf("result %s and expected %s", result, expected)
-		}
+		assertBalance(t, wallet, Euro(10))
 	})
 }
