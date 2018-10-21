8a87917e10a532ed4ffdb8426a3163d2b6b4b597
jlopez <jlopez@qashops>
2018-10-21 15:49:06 +0200

    todo funcionando con punteros

--- 05-wallet/wallet.go
+++ 05-wallet/wallet.go
@@ -6,10 +6,10 @@ type Wallet struct {
 	balance int
 }
 
-func (wallet Wallet) Deposit(amount int) {
+func (wallet *Wallet) Deposit(amount int) {
 	fmt.Printf("In Deposit, address of balance is: %v \n", &wallet.balance)
 	wallet.balance += amount
 }
-func (wallet Wallet) Balance() int {
+func (wallet *Wallet) Balance() int {
 	return wallet.balance
 }
